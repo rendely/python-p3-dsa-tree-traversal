656ce2c293976bdcf5593023ce117c4d0d143b69
matthew rendely <matthew.rendely@gmail.com>
2023-06-25 13:03:42 -0700

    Done

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,14 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_search = [self.root]
+
+    while len(nodes_to_search) > 0:
+      node = nodes_to_search.pop()
+      if node['id'] == id:
+        return node
+      
+      if node['children']:
+        nodes_to_search = nodes_to_search + node['children']
+      
+    return None 
